3eef035c59f0416e4422cc8c039f61cf89e6a7cd
Timo de Kort <dekort.timo@gmail.com>
2022-06-08 21:47:08 +0200

    Add MessageServiceClient

---
+++ src/WebApp/Clients/MessageServiceClient.cs
@@ -0,0 +1,17 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Clients;
+
+public class MessageServiceClient
+{
+    private readonly HttpClient _client;
+
+    public MessageServiceClient(HttpClient client)
+    {
+        _client = client;
+        _client.BaseAddress = new Uri("http://message-service:80/");
+    }
+
+    public async Task SendMessage(SendMessageModel sendMessageModel) =>
+        await _client.PostAsJsonAsync("/", sendMessageModel.Content);
+}
