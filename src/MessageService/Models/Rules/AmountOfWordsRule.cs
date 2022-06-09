ba622881720ecbf014b41504863606225234b4b9
Timo de Kort <dekort.timo@gmail.com>
2022-06-09 20:44:53 +0200

    Implement AmountOfWordsRule with tests

---
+++ src/MessageService/Models/Rules/AmountOfWordsRule.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using MessageService.Factories;
+
+namespace MessageService.Models.Rules;
+
+public class AmountOfWordsRule : AbstractRule
+{
+    private const string SentenceDelimiter = ".";
+    private const string WordDelimiter = " ";
+    
+    private readonly int _maximumAllowedWords;
+    
+    public AmountOfWordsRule(IMessageFactory messageFactory, int maximumAllowedWords) : base(messageFactory)
+    {
+        _maximumAllowedWords = maximumAllowedWords;
+    }
+
+    public override IMessage ValidateMessage(IMessage message)
+    {
+        var sentences = SplitInto(message.Content, SentenceDelimiter).ToList();
+
+        if (sentences.Count == 0)
+        {
+            return message;
+        }
+
+        if (sentences.Count == 1)
+        {
+            var words = SplitInto(sentences[0], WordDelimiter).ToList();
+            return words.Count > _maximumAllowedWords 
+                ? MessageFactory.Create(string.Empty) 
+                : message;
+        }
+
+        var currentCount = 0;
+        var contentBuilder = new StringBuilder();
+        foreach (var sentence in sentences)
+        {
+            var words = SplitInto(sentence, WordDelimiter).ToList();
+
+            if (words.Count + currentCount > _maximumAllowedWords)
+            {
+                break;
+            }
+
+            contentBuilder.Append(sentence);
+            currentCount += words.Count;
+        }
+
+        return new TextMessage(contentBuilder.ToString());
+    }
+
+    private IEnumerable<string> SplitInto(string input, string delimiter) =>
+        Regex.Split(input, @$"(?<=[{delimiter}])", RegexOptions.IgnorePatternWhitespace)
+            .Where(str => !string.IsNullOrWhiteSpace(str));
+}
---
+++ tests/MessageService.Tests/Rules/AmountOfWordsRuleTest.cs
@@ -0,0 +1,133 @@
+﻿using System.Text;
+using MessageService.Factories;
+using MessageService.Models.Rules;
+using NUnit.Framework;
+
+namespace MessageService.Tests.Rules;
+
+[TestFixture]
+public class AmountOfWordsRuleTest
+{
+    private const int MaximumAllowedWords = 5;
+    private TextMessageFactory _textMessageFactory;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _textMessageFactory = new TextMessageFactory();
+    }
+
+    [Test]
+    public void TestNoSentence()
+    {
+        var rule = new AmountOfWordsRule(_textMessageFactory, MaximumAllowedWords);
+        
+        var validatedMessage = rule.ValidateMessage(_textMessageFactory.Create(string.Empty));
+        Assert.IsTrue(validatedMessage.Content == string.Empty);
+    }
+    
+    [Test]
+    public void TestSingleTooLongSentence()
+    {
+        var rule = new AmountOfWordsRule(_textMessageFactory, MaximumAllowedWords);
+
+        var sentence = GenerateSentence(1, MaximumAllowedWords + 1);
+
+        var validatedMessage = rule.ValidateMessage(_textMessageFactory.Create(sentence));
+        Assert.IsTrue(validatedMessage.Content == string.Empty);
+    }
+
+    [Test]
+    public void TestSingleSentence()
+    {
+        var rule = new AmountOfWordsRule(_textMessageFactory, MaximumAllowedWords);
+
+        var sentence = GenerateSentence(1, MaximumAllowedWords);
+
+        var validatedMessage = rule.ValidateMessage(_textMessageFactory.Create(sentence));
+        Assert.IsTrue(sentence == validatedMessage.Content);
+    }
+
+    [Test]
+    public void TestMultipleSentencesTooLong()
+    {
+        var rule = new AmountOfWordsRule(_textMessageFactory, MaximumAllowedWords);
+        var sentence = GenerateSentence(MaximumAllowedWords + 1, 1);
+
+        var validatedMessage = rule.ValidateMessage(_textMessageFactory.Create(sentence));
+
+        var sentenceWordCount = sentence.Split(".").Length - 1;
+        var validatedMessageWordCount = validatedMessage.Content.Split(".").Length;
+        Assert.IsTrue(sentenceWordCount == validatedMessageWordCount);
+    }
+
+    [Test]
+    public void TestMultipleSentences()
+    {
+        var rule = new AmountOfWordsRule(_textMessageFactory, MaximumAllowedWords);
+        var sentence = GenerateSentence(MaximumAllowedWords, 1);
+
+        var validatedMessage = rule.ValidateMessage(_textMessageFactory.Create(sentence));
+
+        var sentenceWordCount = sentence.Split(".").Length;
+        var validatedMessageWordCount = validatedMessage.Content.Split(".").Length;
+        Assert.IsTrue(sentenceWordCount == validatedMessageWordCount);
+    }
+
+    /// <summary>
+    /// Generate specified number of sentences with a specified amount of words
+    /// </summary>
+    /// <param name="sentences">Amount of sentences that need to be generated.</param>
+    /// <param name="words">Amount of words that need to be generated.</param>
+    /// <returns></returns>
+    private string GenerateSentence(int sentences, int words)
+    {
+        var output = new StringBuilder();
+        var sentenceBuilder = new StringBuilder();
+        
+        for (var i = 0; i < sentences; i++)
+        {
+            sentenceBuilder.Clear();
+            
+            for (var j = 0; j < words; j++)
+            {
+                sentenceBuilder.Append($"{RandomWord()}");
+                
+                if (j < words - 1)
+                {
+                    sentenceBuilder.Append(' ');
+                }
+            }
+
+            sentenceBuilder.Append('.');
+
+            if (i < sentences - 1)
+            {
+                sentenceBuilder.Append(' ');
+            }
+            
+            output.Append(sentenceBuilder.ToString());
+        }
+
+        return output.ToString();
+    }
+
+    public static string RandomWord(int min = 3, int max = 5)
+    {
+        const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        var length = GenerateRandomInt(min, max);
+        var chars = new char[length];
+            
+        for (var i = 0; i < length; i++)
+        {
+            chars[i] = characters[GenerateRandomInt(0, characters.Length)];
+        }
+
+        return new string(chars);
+    }
+    
+    private static int GenerateRandomInt(int min = 3, int max = 20)
+    {
+        return Random.Shared.Next(min, max);
+    }
+}
